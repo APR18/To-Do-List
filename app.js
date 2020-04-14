59020573eafa61a5eece686938d8cab0f5af168e
Anandhapadmanabhan R <anandhu.6231@gmail.com>
2020-04-14 17:25:48 +0530

    complete

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-express = require("express");
-bodyParser = require("body-parser");
-//
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+console.log(date);
+
 app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 //
-var items = [];
+const items = [];
+const workItems = [];
 app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = " ";
-  var options = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-  };
-  //
-  day = today.toLocaleDateString("en-US", options);
-  //
-
-  res.render("list", { kindOfDay: day, newListItems: items });
+  day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
-  items.push(item);
+  const item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+  }
+
   res.redirect("/");
 });
 
+// ----------------------WORK PAGE------------------------------------
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work", newListItems: workItems });
+});
+
+// ---------------------ABOUT PAGE-------------------------------------
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
